922f28aa85127a503bd60d2dcbebfb4960748373
mkstowe <mkstowe@umich.edu>
2019-07-17 20:39:55 -0400

    Big changes. Sticky navbar. A lot of functionality. Rewrote style in scss. general bug and style fixes/additions

---
+++ main.js
@@ -0,0 +1,45 @@
+// Change Facebook icon color on hover
+document.getElementById("facebook").onmouseover = function () {
+    document.getElementById("fb-circle").style.color = "#414141";
+};
+
+document.getElementById("facebook").onmouseleave = function () {
+    document.getElementById("fb-circle").style.color = "#2D2D2D";
+};
+
+// Change cart icon on hover
+document.getElementById("cart").onmouseover = function () {
+    this.style.color = "#FCC605";
+};
+
+document.getElementById("cart").onmouseleave = function () {
+    this.style.color = "#F3F3F3";
+};
+
+$(function () {
+    $("#navbar").sticky({topSpacing: 0});
+});
+
+function openTopMenu() {
+    if (window.innerWidth < 680) {
+        document.getElementById("dropdown-content").style.display = "block";
+    }
+}
+
+function openSideMenu() {
+    document.getElementById("dropdown-content-cat").style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (window.innerWidth < 680) {
+        if (event.target !== document.getElementById("dropbtn")) {
+            document.getElementById("dropdown-content").style.display = "none";
+        }
+    }
+
+    if (window.innerWidth < 680) {
+        if (event.target !== document.getElementById("dropbtn-cat")) {
+            document.getElementById("dropdown-content-cat").style.display = "none";
+        }
+    }
+};
