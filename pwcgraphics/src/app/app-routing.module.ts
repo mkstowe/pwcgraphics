2b9e537a1905b6c2a945ff5b3d92c144df43a94d
Michael Stowe <mkstowe@umich.edu>
2022-01-05 00:20:49 -0500

    Main page structure and routing

--- pwcgraphics/src/app/app-routing.module.ts
+++ pwcgraphics/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent, data: { tab: 1 } },
+  { path: 'shop', component: ShopPageComponent, data: { tab: 2 } },
+  { path: 'contact', component: ContactPageComponent, data: { tab: 4 } },
+  { path: 'resources', component: ResourcesComponent, data: { tab: 5 } },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pwcgraphics/src/app/app.module.ts
+++ pwcgraphics/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { ShopPageComponent } from './shop-page/shop-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SocialsComponent } from './socials/socials.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    ShopPageComponent,
+    ContactPageComponent,
+    ResourcesComponent,
+    HomePageComponent,
+    SocialsComponent,
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
